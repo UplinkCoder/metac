3a09f633d7da43ff132453e85045f9cf7dca76f0
Stefan Koch <uplink.coder@googlemail.com>
2022-04-13 12:50:44 +0200

    Fix gen_code.bat

--- gen_code.bat
+++ gen_code.bat
@@ -1,3 +1,4 @@
 mkdir generated
-cl /TP /I.. utils\gen_metac_match_keyword.c /Os
+
+cl /TP /Iutils utils\gen_metac_match_keyword.c /Os /I.
 gen_metac_match_keyword.exe > generated\metac_match_keyword.inl
