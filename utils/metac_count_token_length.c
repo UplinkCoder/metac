5c24a529003b9816022be7bfa7716fe24a5241e6
Stefan Koch <uplink.coder@googlemail.com>
2022-04-13 13:52:44 +0200

    Add tool to generate StaticMetaCTokenLength switch

---
+++ utils/metac_count_token_length.c
@@ -0,0 +1,65 @@
+#include "../compat.h"
+#include "../metac_lexer.c"
+#include <stdio.h>
+
+void CountTokenLengths();
+
+int main(int argc, char* argv[])
+{
+    CountTokenLengths();
+}
+
+void CountTokenLengths()
+{
+    uint32_t token_len[10] = {0};
+
+#define TOK_SELF(TOK) \
+    TOK
+
+    const metac_token_enum_t first_keyword = FIRST_KEYWORD_TOKEN(TOK_SELF);
+    const metac_token_enum_t last_keyword = LAST_KEYWORD_TOKEN(TOK_SELF);
+
+#define TOK_IS_KW(TOK) \
+    (TOK >= first_keyword && TOK <= last_keyword)
+
+#define KW_LEN(TOK) \
+    ((unsigned int)(sizeof(#TOK) - sizeof("tok_kw_")))
+
+#define TOK_LENGTH(TOK) \
+    ( ((TOK_IS_KW(TOK)) ? (KW_LEN(TOK)) : StaticMetaCTokenLength(TOK)) )
+
+#define COUNT_TOK(TOK) \
+    token_len[TOK_LENGTH(TOK)]++;
+
+    FOREACH_STATIC_TOKEN(COUNT_TOK)
+
+    uint32_t currentMaxLenCount = 0;
+    uint32_t currentMaxLenCanidate = 0;
+
+    for(uint32_t* len = &token_len[0];
+       len < (token_len + (sizeof(token_len) / sizeof(token_len[0])));
+        len++)
+    {
+        unsigned int idx = (unsigned int)(len - token_len);
+        if (currentMaxLenCount < *len)
+        {
+            currentMaxLenCanidate = idx;
+            currentMaxLenCount = *len;
+        }
+        fprintf(stderr, "  %u tokens have length: %u\n", *len, idx);
+    }
+
+    fprintf(stderr, "MaxCountedLength: %u\n", currentMaxLenCanidate);
+
+    printf("switch(tok)\n");
+    printf("{\n");
+    printf("    default : return %u;\n\n", currentMaxLenCanidate);
+
+#define SWITCH_PRINT(TOK) \
+    if (TOK_LENGTH(TOK) != currentMaxLenCanidate) \
+    printf("    case %s : return %u;\n", #TOK, TOK_LENGTH(TOK));
+
+    FOREACH_STATIC_TOKEN(SWITCH_PRINT)
+
+    printf("}\n");
+}
