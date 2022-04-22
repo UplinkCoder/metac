7ef540a6271bf56ff75d1bf0da504c53eaa9253c
Stefan Koch <uplink.coder@googlemail.com>
2022-04-22 03:07:46 +0200

    same for run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,2 +1,5 @@
-cl /TP metac_lexer.c /DTEST_LEXER /Os
+cl /TP metac_lexer.c /DTEST_LEXER /Og /Zi
 metac_lexer.exe
+
+cl /TP metac_parser.c /DACCEL=ACCEL_TABLE /DTEST_PARSER /Og /Zi
+metac_parser.exe
