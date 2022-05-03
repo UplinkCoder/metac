500dc240f168ee050a8e9f40afaecc7c12954173
Stefan Koch <uplink.coder@googlemail.com>
2022-05-03 22:38:30 +0200

    Improved generated match keyword code Such that it no longer relies on the compiler figuring out the memcmp

--- utils/gen_metac_match_keyword.c
+++ utils/gen_metac_match_keyword.c
@@ -2,6 +2,13 @@
 #include "../cache/crc32.c"
 #include "../metac_lexer.h"
 
+const char* C4Macros =
+"#define C4(A, B, C, D) \\\n"
+"    ((uint32_t)(A | B << 8 | C << 16 | D << 24)) \\\n"
+"\n\n"
+"#define C2(A, B) \\\n"
+"    ((uint16_t)(A | B << 8))\n";
+
 #include <stdio.h>
 /*
     switch (tok->IdentifierKey)
@@ -11,21 +18,63 @@
             tok->TokenType = tok_kw_struct;
     break;
 
-
+*/
 void WriteCmp(const char* kw, uint32_t kw_len)
 {
 	printf("if (");
 
 	uint32_t kw_pos = 0;
-	while (kw_len > 4)
+	while (kw_len >= 4)
 	{
-		printf("identifier")
+        uint32_t index = (kw_pos / 4);
+        kw_len -= 4;
 
+        const char c1 = kw[kw_pos + 0];
+        const char c2 = kw[kw_pos + 1];
+        const char c3 = kw[kw_pos + 2];
+        const char c4 = kw[kw_pos + 3];
+
+        if (kw_pos != 0)
+            printf("\n && ");
+        else
+            printf("\n    ");
+		printf("(*(((uint32_t*)identifier) + %u)) == C4('%c', '%c', '%c', '%c')",
+                                                  index,      c1,   c2,   c3,   c4);
+        kw_pos += 4;
 	}
 
-	printf(")");
+    if (kw_pos && kw_len)
+        printf("\n && ");
+    else
+        printf("\n    ");
+    switch(kw_len)
+    {
+        case 3 :
+        {
+            const char c1 = kw[kw_pos];
+            const char c2 = kw[kw_pos + 1];
+            const char c3 = kw[kw_pos + 2];
+            printf("(*(((uint16_t*)identifier) + %u)) == C2('%c', '%c')\n",
+                                                 kw_pos / 2,  c1,  c2);
+            printf(" && ((*(identifier + %u)) == '%c')\n", kw_pos + 2, c3);
+        } break;
+        case 2 :
+        {
+            const char c1 = kw[kw_pos];
+            const char c2 = kw[kw_pos + 1];
+            printf("(*(((uint16_t*)identifier) + %u)) == C2('%c', '%c')\n",
+                                                 kw_pos / 2,  c1,  c2);
+        } break;
+        case 1 :
+        {
+            const char c1 = kw[kw_pos];
+            printf("((*(identifier + %u)) == '%c')\n", kw_pos, c1);
+        }
+    }
+
+	printf(")\n");
 }
-*/
+
 void WriteMatchFunction(void)
 {
 #define KW_PREFIX \
@@ -46,6 +95,8 @@ void WriteMatchFunction(void)
 #define KW_STR(KW) \
     #KW + KW_PREFIX_LEN
 
+    printf("%s\n", C4Macros);
+
 #define KW_WRITE_DEFINE(KW) \
     printf("#define %s_key 0x%x\n", \
         KW_STR(KW), KW_KEY(KW));
@@ -65,9 +116,11 @@ void WriteMatchFunction(void)
     printf("    {\n");
 //    WriteCmp(#KW + KW_PREFIX_LEN, KW_LEN(KW)); \
 
+//    printf("    if(!memcmp(identifier, \"%s\", %u))\n", KW_STR(KW), KW_LEN(KW));
+
 #define KW_WRITE_CASE(KW) \
     printf("    case %s_key :\n", KW_STR(KW)); \
-    printf("    if(!memcmp(identifier, \"%s\", %u))\n", KW_STR(KW), KW_LEN(KW)); \
+    WriteCmp(KW_STR(KW), KW_LEN(KW)); \
     printf("            tok->TokenType = %s;\n", #KW); \
     printf("    break;\n");
 
