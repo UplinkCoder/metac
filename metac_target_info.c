a7fa6730073308a696604c48715e63efd5b5f342
Stefan Koch <uplink.coder@googlemail.com>
2022-05-20 02:42:30 +0200

    Fix metac_target_info.c

--- metac_target_info.c
+++ metac_target_info.c
@@ -38,7 +38,7 @@ uint32_t MetaCTargetInfo_GetBasicSize(const metac_target_info_t* targetInfo,
             result = targetInfo->SizeLongDouble;
         break;
     }
- 
+
     return result;
 }
 
@@ -51,25 +51,35 @@ uint32_t MetaCTargetInfo_GetBasicAlign(const metac_target_info_t* targetInfo,
     {
         case basic_bool:
             result = targetInfo->AlignmentBool;
+        break;
         case basic_char:
             result = targetInfo->AlignmentChar;
+        break;
         case basic_short:
             result = targetInfo->AlignmentShort;
+        break;
         case basic_int:
             result = targetInfo->AlignmentInt;
+        break;
         case basic_long:
             result = targetInfo->AlignmentLong;
+        break;
         case basic_long_long:
             result = targetInfo->AlignmentLongLong;
+        break;
         case basic_size_t:
             result = targetInfo->AlignmentSizeT;
+        break;
         case basic_float:
             result = targetInfo->AlignmentFloat;
+        break;
         case basic_double:
             result = targetInfo->AlignmentDouble;
+        break;
         case basic_long_double:
-            result = targetInfo->AlignmentLongDouble;                
+            result = targetInfo->AlignmentLongDouble;
+        break;
     }
-    
+
     return result;
 }
