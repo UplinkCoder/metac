2cea5a2855db6afa9b12efb174aa0bbf916b2e47
Stefan Koch <uplink.coder@googlemail.com>
2022-06-25 18:02:16 +0200

    fx header exprsem

--- metac_expr_semantic.h
+++ metac_expr_semantic.h
@@ -1,3 +1,5 @@
+#include "metac_task.h"
+
 typedef struct MetaCSemantic_doExprSemantic_task_context_t
 {
     metac_semantic_state_t* Sema;
@@ -13,6 +15,9 @@ metac_sema_expression_t* MetaCSemantic_doExprSemantic_(metac_semantic_state_t* s
                                                        metac_expression_t* expr,
                                                        const char* callFile,
                                                        uint32_t callLine);
+void MetaCSemantic_doExprSemantic_Task(task_t* task);
 
 void MetaCSemantic_PushExpr(metac_semantic_state_t* self, metac_sema_expression_t* expr);
 void MetaCSemantic_PopExpr(metac_semantic_state_t* self,  metac_sema_expression_t* expr);
+
+bool MetaCSemantic_CanHaveAddress(metac_semantic_state_t* self, metac_expression_t* expr);
