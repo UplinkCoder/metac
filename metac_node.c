4bea4c17e07977744292b2e26a62ea9c32c1b834
Stefan Koch <uplink.coder@googlemail.com>
2022-06-29 03:17:52 +0200

    Add metac_node.c

---
+++ metac_node.c
@@ -0,0 +1,16 @@
+#include "metac_node.h"
+
+static inline bool MetaCNode_IsExpression(metac_node_t node)
+{
+    return (node->Kind > node_exp_invalid && node->Kind < node_exp_max);
+}
+
+static inline bool MetaCNode_IsStatement(metac_node_t node)
+{
+    return (node->Kind > stmt_min && node->Kind < stmt_max);
+}
+
+static inline bool MetaCNode_IsDeclaration(metac_node_t node)
+{
+    return (node->Kind > decl_min && node->Kind < decl_max);
+}
