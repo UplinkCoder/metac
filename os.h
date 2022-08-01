8d6b44f91d7d9cea6cfe953e241052a18920f127
Stefan Koch <uplink.coder@googlemail.com>
2022-08-01 15:00:50 +0200

    Work on the OS api

--- os.h
+++ os.h
@@ -8,14 +8,65 @@
 #endif
 
 #ifdef _MSC_VER
-#    <memoryapi.h>
+#  include <winbase.h>
+#  define WINDOWS
 #endif
+
+typedef enum file_mode_t
+{
+    FileMode_Read,
+    FileMode_Write,
+    FileMode_ReadWrite
+} file_mode_t;
+
+typedef enum os_error_t
+{
+    Error_OutOfMemory,
+
+    Error_FileDoesNotExist,
+    Error_FileNotReadable,
+    Error_FileNotWritable,
+    Error_FileShortRead,
+    Error_FileShortWrite,
+
+    Error_NotEnoughSpace,
+} os_error_t;
+
+
 
 struct OS
 {
     uint32_t PageSize;
-    
-    void* (*PageAlloc)(uint32_t minSize, uint32_t* allocatedSize);
+
+    os_error_t (*PageAlloc)(uint32_t minSize, uint32_t* allocatedSize, void** outMemory);
+
+    /// Opens a file
+    os_error_t (*FileOpen)(const char* path, file_mode_t mode, fhandle* outFile);
+
+    ///
+    os_error_t (*FileOpenAsync)(const char* path, file_mode_t mode,
+                                void (*FileOpenCb) (fhandle handle, void* userPointer), void* userPointer);
+    /// Query size of a file
+    os_error_t (*FileSize) (fhandle file, uint32_t* outSizeLow, uint32_t* outSizeHigh);
+
+    /// Reads data from a file it will advance an internal pointer such that
+    /// a following call to FileRead will read from where the last call left off
+    /// In the event of a short read it updates inoutSize to reflect the number of bytes read
+    os_error_t (*FileRead)(fhandle file, uint32_t offset, uint32_t *inoutSize, void *outBuffer);
+
+    /// Writes data to a file it will advance an internal pointer such that
+    /// another call to FileWrite will write to where the last call left off
+    /// In the event of a short write it updates inoutSize to reflect the number of bytes written
+    os_error_t (*FileWrite)(fhandle file, uint32_t offset, uint32_t inoutSize, void *inBuffer);
+
+    /// Report the absolute position in the file
+    os_error_t (*FileTellPosition)(fhandle file, uint32_t* loWord, uint32_t* hiWord);
+
+    /// Set the read and write pointer to a given position.
+    os_error_t (*FileSetPostion)(fhandle file, uint32_t loWord, uint32_t hiWord);
+
+    // Closes the file and flushes any pending write/operations
+    void (*FileClose)(fhandle handle);
 };
 
 extern struct OS OS;
