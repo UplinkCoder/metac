7f491882a60ba95676e68001a308254fd4411309
Stefan Koch <uplink.coder@googlemail.com>
2022-09-23 10:15:35 +0200

    Add excerpt from stdint.h into compat.h

--- compat.h
+++ compat.h
@@ -15,15 +15,32 @@
 #  define snprintf _snprintf
 #endif
 
-#if defined(_MSC_VER)
-#  include "stdint_msvc.h"
+#ifndef HAVE_STDINT_H
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+
+#  if defined(_MSC_VER)
+    typedef signed __int64       int64_t;
+    typedef unsigned __int64     uint64_t
+#  else
+    typedef signed long int int64_t;
+    typedef unsigned long int uint64_t;
+#  endif
 #  define UINT32_MAX (cast(uint32_t)0xffffffff)
 #  define UINT16_MAX (cast(uint16_t)0xffff)
+
+#else
+#  include <stdint.h>
+#endif
+
+#if defined(_MSC_VER)
 #  ifndef __cplusplus
 #    error "win32 compile only works in c++ mode ... use /TP"
 #  endif
-#else
-#  include <stdint.h>
 #endif
 
 #if !defined( __STDC_VERSION__ ) || __STDC_VERSION__ < 201112L
