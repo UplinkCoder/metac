e66c0b489aab1f65c9251e407027cd09bf8e1570
Stefan Koch <uplink.coder@googlemail.com>
2022-10-14 19:43:26 +0200

    Add in-process debugging server

---
+++ debug/debug_server.c
@@ -0,0 +1,235 @@
+#ifdef DEBUG_SERVER
+
+#include "../debug/debug_server.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+debug_server_t* g_DebugServer;
+
+#ifndef ARRAYSIZE
+#define ARRAYSIZE(ARR) \
+    (sizeof ((ARR)) / sizeof(*(ARR)) / !(sizeof((ARR)) % sizeof(*(ARR))))
+#endif
+
+#ifndef WIN32
+int fopen_s (FILE ** fileP, const char *path, const char *mode)
+{
+    (*fileP) = fopen (path, mode);
+    return (*fileP != 0);
+}
+#endif
+
+static int send_html(struct MHD_Connection* connection, const char* text, int len)
+{
+    struct MHD_Response* response;
+    int ret;
+    response =
+        MHD_create_response_from_buffer(len, text, MHD_RESPMEM_PERSISTENT);
+    MHD_add_response_header(response, MHD_HTTP_HEADER_CONTENT_TYPE,
+        "text/html");
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+    return ret;
+}
+
+static int serveFile(const char* filename, const char* contentType, struct MHD_Connection* conn)
+{
+    FILE* f;
+    char pageBuffer[8192];
+    int sz;
+    char* page;
+
+    struct MHD_Response* response;
+    int ret;
+
+    fopen_s(&f, filename, "r");
+    fseek(f, 0, SEEK_END);
+    sz = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    if (sz < 8192)
+        page = pageBuffer;
+    else
+        page = (char*)malloc(sz + 1);
+
+    fread(page, 1, sz, f);
+    page[sz] = '\0';
+    // printf("page: %s\n", page);
+    fclose(f);
+
+    response =
+        MHD_create_response_from_buffer(sz, (void*)page,
+            MHD_RESPMEM_MUST_COPY);
+    MHD_add_response_header(response, MHD_HTTP_HEADER_CONTENT_TYPE,
+        contentType);
+    ret = MHD_queue_response(conn, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+
+    return ret;
+}
+
+MHD_HANDLER (handleAllocators)
+{
+    static char responseString[4096];
+    debug_server_t* debugServer = (debug_server_t*) cls;
+    int i;
+    int p = 0;
+    int len;
+    int n_fields = 0;
+    
+    char body[4096];
+    body[0] = '\0';
+
+    const char* headers[] = {
+        "File",
+        "Line",
+        "ArenaCount",
+        "AreansUsed",
+        "Used",
+        "Allocated"
+    };
+
+    p += snprintf(body + p, ARRAYSIZE(body) - p, "<tr>");
+    for(i = 0; i < ARRAYSIZE(headers); i++)
+    {
+        p += snprintf(body + p, ARRAYSIZE(body) - p, "<th>%s</th>", headers[i]);
+    }
+    p += snprintf(body + p, ARRAYSIZE(body) - p, "</tr>");
+
+    for (i = 0; i < debugServer->AllocatorsCount; i++)
+    {
+        int j;
+        int vp = 0;
+        uint32_t usedSize = 0;
+        uint32_t allocatedSize = 0;
+        uint32_t arenasUsed = 0;
+        
+        metac_alloc_t * alloc = debugServer->Allocators[i];
+        p += snprintf (body + p, ARRAYSIZE (body) - p, "<tr>", alloc);
+    
+        for(j = 0; j < alloc->ArenaCount; j++)
+        {
+            usedSize += alloc->Arenas[j].Offset;
+            allocatedSize += (alloc->Arenas[j].Offset + alloc->Arenas[j].SizeLeft);
+            if (alloc->Arenas[j].Offset)
+            {
+                arenasUsed++;
+            }
+        }
+
+        {
+            p += snprintf (body + p, ARRAYSIZE (body) - p,
+                           "<td>%s</td>", ((!(alloc->FileID.v < 1 || alloc->FileID.v > g_filenames.StringMemorySize)
+                                           ) ? g_filenames.StringMemory + (alloc->FileID.v - 4) : "0")
+            );
+            
+            p += snprintf (body + p, ARRAYSIZE (body) - p,
+                           "<td>%u</td>", alloc->Line
+            );
+            
+            p += snprintf (body + p, ARRAYSIZE (body) - p,
+                           "<td>%u</td>", alloc->ArenaCount
+            );
+            
+            p += snprintf (body + p, ARRAYSIZE (body) - p,
+                           "<td>%u</td>", arenasUsed
+            );
+            
+            p += snprintf (body + p, ARRAYSIZE (body) - p,
+                           "<td>%u</td>", usedSize
+            );
+            
+            p += snprintf (body + p, ARRAYSIZE (body) - p,
+                           "<td>%u</td>", allocatedSize
+            );
+        }
+
+        p += snprintf(body + p, ARRAYSIZE(body) - p, "</tr>");
+    }
+
+   len = snprintf (responseString, ARRAYSIZE (responseString),
+                    "<hmtl><body>"
+                    "<h3>Allocators: </h3>"
+                    "<table id=\"t1\">%s</table>"
+                    "</body></hmtl>",
+           body);
+    return send_html (connection, responseString, len);
+}
+
+static MHD_HANDLER(debugServerHandler)
+{
+    debug_server_t *debugServer = cast(debug_server_t*) cls;
+
+    if (strcmp(url, "/allocators") == 0)
+    {
+        return handleAllocators(MHD_HANDLER_PASSTHROUGH);
+    }
+
+    if (debugServer->Handler)
+    {
+        return debugServer->Handler(MHD_HANDLER_PASSTHROUGH);
+    }
+
+    return MHD_NO;
+}
+
+static MHD_COMPLETED_CB (MhdCompletionCallback)
+{
+}
+
+
+int Debug_Init(debug_server_t* debugServer, unsigned short port) {
+    struct MHD_Daemon *d;
+#ifdef WIN32
+    WSADATA wd;
+    if (WSAStartup (MAKEWORD (2, 2), &wd) != 0)
+    {
+        fprintf (stderr, "FATAL: failed to initialize Windows Sockets.\n");
+        return 1;
+    }
+#endif    /**/
+
+    d = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, port,
+                          NULL, NULL,
+                          debugServerHandler, debugServer,
+                          MHD_OPTION_NOTIFY_COMPLETED,
+                            MhdCompletionCallback, 0,
+                          MHD_OPTION_END);
+
+    debugServer->Daemon = d;
+
+    uint32_t allocatorCapa = 64;
+    uint32_t allocationCapa = 256;
+    
+    debugServer->Allocators = (metac_alloc_t**)
+        malloc(sizeof(metac_alloc_t*) * allocatorCapa);
+    debugServer->AllocatorsCount = 0;
+    debugServer->AllocatorsCapacity = allocatorCapa;
+
+    debugServer->Allocations = (debug_allocation_t*) 
+        malloc(sizeof(debug_allocation_t) * allocationCapa);
+    debugServer->AllocationsCount = 0;
+    debugServer->AllocationsCapacity = allocationCapa;
+    
+    return 0;
+}
+
+void Debug_Allocator(debug_server_t* debugServer, metac_alloc_t* allocator)
+{
+    uint32_t count = debugServer->AllocatorsCount++;
+    debugServer->Allocators[count] = allocator;
+}
+
+void Debug_Allocation(debug_server_t* debugServer, metac_alloc_t* allocator, uint32_t sz, const char* file, uint32_t line)
+{
+    uint32_t count = debugServer->AllocationsCount++;
+    
+    debugServer->Allocations[count].size = sz;
+    debugServer->Allocations[count].file = file;
+    debugServer->Allocations[count].line = line;
+}
+
+void Debug_Pump(debug_server_t* debugServer)
+{
+}
+
+#endif
---
+++ debug/debug_server.h
@@ -0,0 +1,69 @@
+#ifndef _DEBUG_SERVER_H_
+#define _DEBUG_SERVER_H_
+#ifdef DEBUG_SERVER
+
+#include "microhttpd.h"
+
+#include "../os/metac_alloc.h"
+
+#define MHD_COMPLETED_CB(NAME) \
+void NAME (void *cls, \
+           struct MHD_Connection *connection, \
+           void **con_cls, \
+           enum MHD_RequestTerminationCode toe)
+
+#define MHD_HANDLER(NAME) \
+int NAME (void *cls, \
+          struct MHD_Connection *connection, \
+          const char *url, \
+          const char *method, \
+          const char *version, \
+          const char *upload_data, \
+          size_t * upload_data_size, \
+          void **con_cls)
+
+#define MHD_HANDLER_PASSTHROUGH \
+    cls, connection, \
+    url, method, version, \
+    upload_data, upload_data_size, \
+    con_cls
+
+typedef MHD_HANDLER ((*mhd_handler_t));
+
+typedef struct debug_allocation_t
+{
+    uint32_t size;
+    const char* file;
+    uint32_t line;
+} debug_allocation_t;
+
+typedef struct debug_server_t
+{
+    struct MHD_Daemon* Daemon;
+    mhd_handler_t Handler;
+    metac_alloc_t** Allocators;
+    uint32_t AllocatorsCount;
+    uint32_t AllocatorsCapacity;
+
+    debug_allocation_t* Allocations;
+    uint32_t AllocationsCount;
+    uint32_t AllocationsCapacity;
+} debug_server_t;
+
+int Debug_Init(debug_server_t* debugServer, unsigned short port);
+void Debug_Pump(debug_server_t* debugServer);
+
+void Debug_Allocator(debug_server_t* debugServer, metac_alloc_t* allocator);
+void Debug_Allocation(debug_server_t* debugServer, metac_alloc_t* allocator, uint32_t sz, const char* file, uint32_t line);
+extern debug_server_t* g_DebugServer;
+
+#else
+
+#define Debug_Init(S, P)
+#define Debug_Pump(S)
+#define Debug_Allocator(S, A)
+#define Debug_Allocation(S, A, Z, F, L)
+
+#endif
+
+#endif
