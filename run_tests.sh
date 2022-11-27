5efab4f7b3deec5499a37a11eefbac5b9615b3c1
Per Nordlöw <per.nordlow@gmail.com>
2022-11-27 13:46:00 +0100

    Respect CFLAGS in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -6,10 +6,10 @@ fi
 
 ./gen_code.sh
 
-$CC  parser/metac_lexer.c hash/crc32c.c os/os.c os/metac_alloc.c -g3 -DTEST_LEXER -o lexer_test
+$CC $CFLAGS parser/metac_lexer.c hash/crc32c.c os/os.c os/metac_alloc.c -g3 -DTEST_LEXER -o lexer_test
 ./lexer_test
 
-$CC parser/metac_parser_obj.c driver/metac_lpp.c \
+$CC $CFLAGS parser/metac_parser_obj.c driver/metac_lpp.c \
     -g3 -DNO_DOT_PRINTER -DTEST_PARSER -DNO_SEMANTIC -DNO_FIBERS -DNO_PREPROCESSOR \
     -o parser_test
 ./parser_test
