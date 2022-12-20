473358bd6f412729b4bc076b143ef179eb832920
Stefan Koch <uplink.coder@googlemail.com>
2022-12-20 14:41:30 +0100

    repl/build.sh
    have repl build script take cflags into account

--- repl/build.sh
+++ repl/build.sh
@@ -13,7 +13,7 @@ if [ -z $ACCEL ]; then
     ACCEL=ACCEL_TABLE
 fi
 
-$CC linenoise_repl.c \
+$CC $CFLAGS linenoise_repl.c \
     -DACOSW_EXTERNAL_ASM ../3rd_party/libaco/acosw.S \
     -I.. -DACCEL=$ACCEL \
     -g3 -O0 -march=native -mtune=native \
