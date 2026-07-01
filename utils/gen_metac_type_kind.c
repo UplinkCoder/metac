09ad869d7ada057bfe5b71aaa491e71f411aaf96
Stefan Koch <uplink.coder@googlemail.com>
2026-07-01 10:34:33 +0200

    Add toChars to generated compiler interface code

--- utils/gen_metac_type_kind.c
+++ utils/gen_metac_type_kind.c
@@ -18,53 +18,81 @@ int main(int argc, const char* argv[])
 // #define FOREACH_TYPE_INDEX_KIND(M) \
 //    M(type_index_unknown       , 0x0) \
 
-    FILE *f = fopen("../compiler_intrinsics/metac_type_kind.h","w+");
-
     const char* INCLUDE_GUARD_NAME = "_METAC_TYPE_KIND_H_";
     const char* enumName = "metac_type_kind_t";
     uint32_t maxKindNameLength = 0;
+    uint32_t typeKindCount = 0;
     char* indentString;
+    char const * *typeKindNames;
 
-    fprintf(f, "#ifndef %s\n", INCLUDE_GUARD_NAME);
-    fprintf(f, "#define %s\n", INCLUDE_GUARD_NAME);
-
-    fprintf(f, "typedef enum %s {\n", enumName);
-
-
-    for(type_kind_info *typeKindInfo = typeKindInfos;
-        typeKindInfo->KindString; typeKindInfo++)
     {
-        unsigned int kindStringLength = strlen(typeKindInfo->KindString);
-        if (maxKindNameLength < kindStringLength)
+        for(type_kind_info *typeKindInfo = typeKindInfos;
+            typeKindInfo->KindString; typeKindInfo++)
         {
-            maxKindNameLength = kindStringLength;
+            unsigned int kindStringLength = strlen(typeKindInfo->KindString);
+            if (maxKindNameLength < kindStringLength)
+            {
+                maxKindNameLength = kindStringLength;
+            }
+            typeKindCount++;
         }
+
+        indentString = malloc(maxKindNameLength + 1);
+        memset(indentString, ' ', maxKindNameLength);
+        indentString[maxKindNameLength] = '\0';
     }
+    typeKindNames = malloc(sizeof(char *) * typeKindCount);
 
-    indentString = malloc(maxKindNameLength + 1);
-    memset(indentString, ' ', maxKindNameLength);
-    indentString[maxKindNameLength] = '\0';
-
-    for(type_kind_info *typeKindInfo = typeKindInfos;
-        typeKindInfo->KindString; typeKindInfo++)
     {
-        const char* kindString = typeKindInfo->KindString;
-        int kindValue = typeKindInfo->KindValue;
-        unsigned int kindStringLength = strlen(kindString);
+#define HEADER_PATH "../compiler_intrinsics/metac_type_kind.h"
+        FILE *header = fopen(HEADER_PATH ,"w+");
 
-        char firstChar = (char)toupper(kindString[0]);
-        fprintf(f, "    TypeKind_%c%s %s= 0x%X,\n", firstChar, kindString + 1, indentString + kindStringLength, kindValue);
+        fprintf(header, "#ifndef %s\n", INCLUDE_GUARD_NAME);
+        fprintf(header, "#define %s\n", INCLUDE_GUARD_NAME);
+        fprintf(header, "typedef enum %s {\n", enumName);
+
+        {
+            uint32_t i = 0;
+            for(type_kind_info *typeKindInfo = typeKindInfos;
+                typeKindInfo->KindString; typeKindInfo++)
+            {
+                const char* kindString = typeKindInfo->KindString;
+                int kindValue = typeKindInfo->KindValue;
+                unsigned int kindStringLength = strlen(kindString);
+                char firstChar = (char)toupper(kindString[0]);
+                uint32_t kindNameLength = snprintf(0, 0, "TypeKind_%c%s", firstChar, kindString + 1);
+                char* kindNameString = (char*)(typeKindNames[i++] = malloc(kindNameLength + 1));
+                sprintf(kindNameString, "TypeKind_%c%s", firstChar, kindString + 1);
+
+                fprintf(header, "    %s %s= 0x%X,\n", kindNameString, indentString + kindStringLength, kindValue);
+            }
+        }
+
+        fprintf(header, "} %s;\n", enumName);
+        fprintf(header, "#endif // %s\n", INCLUDE_GUARD_NAME);
+        fclose(header);
     }
 
-// #define PRINT_MEMBER(MEMBER_NAME, MEMBER_VALUE) \
-//        fprintf(f, "    TypeKind_%c%s\t\t\t= 0x%X,\n", toupper((#MEMBER_NAME + 11)[0]), #MEMBER_NAME + 12, MEMBER_VALUE);
+    {
+        FILE *code = fopen("../compiler_intrinsics/metac_type_kind.c","w+");
 
-    fprintf(f, "} %s;\n", enumName);
+        fprintf(code, "#include \"" HEADER_PATH "\"\n");
+        fprintf(code, "const char* MetaCTypeKind_toChars(%s value)\n", enumName);
+        fprintf(code, "{\n");
+        fprintf(code, "  const char* result = 0;\n");
+        fprintf(code, "  switch(value) {\n");
+        {
+            uint32_t i = 0;
+            for(i = 0; i < typeKindCount; i++)
+            {
+                const char* mName =  typeKindNames[i];
+                fprintf(code, "    case %s: result = \"%s\"; break;\n", mName, mName);
+            }
+        }
+        fprintf(code, "  }\n");
+        fprintf(code, "  return result;\n");
+        fprintf(code, "}\n");
 
-    fprintf(f, "#endif // %s\n", INCLUDE_GUARD_NAME);
-
-    fclose(f);
-
+        fclose(code);
+    }
 }
-
-
